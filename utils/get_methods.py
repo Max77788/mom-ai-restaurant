0c0d431c7f597d5dab4d888c5c10ea71a6934c0d
Maxik <weee@mail.com>
2024-05-26 16:07:55 +0000

    Added requirements.txt and Procfile

--- utils/get_methods.py
+++
@@ -1,8 +0,0 @@
-from pprint import pprint
-
-from rapyd_utilities import make_request
-
-response = make_request(method='get',
-                        path=f'/v1/payment_methods/countries/US')
-
-pprint(response)
