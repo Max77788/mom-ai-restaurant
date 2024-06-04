813341b1c491927bb18220b54c9fe7eed11b474a
Maxik <weee@mail.com>
2024-06-04 17:23:31 +0000

    Switched to radio buttons

--- utils/forms.py
+++ utils/forms.py
@@ -1,10 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FileField, SubmitField
-from flask_wtf import FlaskForm
-from wtforms import StringField, FileField, SubmitField, PasswordField
-from flask_wtf import FlaskForm
-from wtforms import StringField, FileField, SubmitField, ValidationError
-from wtforms.validators import Email, DataRequired, URL, EqualTo, Length
+from wtforms import StringField, FileField, SubmitField, PasswordField, SelectField, ValidationError, IntegerField, RadioField
+from wtforms.validators import Email, DataRequired, URL, EqualTo, Length, Optional
 
 def OptionalURL(message=None):
     def _optional_url(form, field):
@@ -16,17 +12,34 @@ def OptionalURL(message=None):
 class RestaurantForm(FlaskForm):
     restaurant_name = StringField('Restaurant Name', validators=[DataRequired()])
     restaurant_url = StringField('Restaurant Website', validators=[
-        OptionalURL()])
+        URL()])
     #restaurant_url = StringField('Restaurant Website', validators=[
         #DataRequired(), URL(require_tld=True, message="Invalid URL. Please enter a valid URL.")])
     email = StringField('Email', validators=[DataRequired(), Email()])
     menu = FileField('Upload Menu')#, validators=[DataRequired()])
+    currency = RadioField('Currency of your restaurant', choices=[('USD', 'US Dollar'), ('EUR', 'Euro')], validators=[DataRequired()])
     #script = FileField('Upload Script', validators=[DataRequired()])
     #other_instructions = StringField('Other Instructions (Optional)')
+    
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
     confirm_password = PasswordField('Confirm Password', validators=[
         DataRequired(), EqualTo('password', message='Passwords must match.')])
     submit = SubmitField('Register')
+
+def validate_seven_digit_number(form, field):
+    if len(str(field.data)) < 7:
+        raise ValidationError('The number must be at least 7 digits long.')
+
+class RestaurantFormUpdate(FlaskForm):
+    name = StringField('Restaurant Name', validators=[Optional()])
+    website_url = StringField('Restaurant Website', validators=[Optional(),
+        URL()])
+    Add_MOM_AI_bot_chat = IntegerField('MOM AI Bot ID', validators=[Optional(), validate_seven_digit_number])
+
+class UpdateMenuForm(FlaskForm):   
+    menu_update = FileField('Upload New Menu', validators=[DataRequired()])
+    submit = SubmitField('Update Menu')
+    
 
 class ConfirmationForm(FlaskForm):
     confirmation_code = StringField('Confirmation Code', validators=[DataRequired()])
