4fbafeb92e3ea7ac085090c983f0da8f7636c38d
Maxik <weee@mail.com>
2024-08-07 16:22:42 +0000

    Added url slug in posts

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ db = SQLAlchemy(app)
 # Define the Post model
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    url_slug = db.Column(db.String(150), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
 
